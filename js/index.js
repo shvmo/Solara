9930384d710d1f07dc901921337330a8efb80818
shvmo <15221270@qq.com>
2026-08-15 17:13:42 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -650,6 +650,7 @@ function buildAudioProxyUrl(url) {
 const SOURCE_OPTIONS = [
     { value: "netease", label: "网易云音乐" },
     { value: "kuwo", label: "酷我音乐" },
+    { value: "tencent", label: "QQ音乐" },
     { value: "joox", label: "JOOX音乐" },
     { value: "bilibili", label: "哔哩哔哩" },
 ];
