dc2456bba768ffa3f44837625209bbd44723637b
shvmo <15221270@qq.com>
2026-08-15 16:55:38 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -650,9 +650,9 @@ function buildAudioProxyUrl(url) {
 const SOURCE_OPTIONS = [
     { value: "netease", label: "网易云音乐" },
     { value: "kuwo", label: "酷我音乐" },
-    { value: "kugou", label: "酷狗音乐" },
+    { value: "tencent", label: "QQ音乐" },
     { value: "joox", label: "JOOX音乐" },
-    { value: "bilibili", label: "哔哩哔哩" }
+    { value: "bilibili", label: "哔哩哔哩" },
 ];
 
 function normalizeSource(value) {
